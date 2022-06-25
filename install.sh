8baa7802faf866c9bf06b6568dee81b1095aca83
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-06-25 19:14:09 +0100

    Not all Codespaces have bash; use sh

--- install.sh
+++ install.sh
@@ -1,24 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "Installing Linux packages..."
+
   # GitHub CLI
-  # Install GitHub CLI
   curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
   sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
   sudo apt -y update
-  sudo apt -y install build-essential gh
+  sudo apt -y install build-essential gh bat exa
   sudo apt -y upgrade
-
-  sudo apt update
-
 fi
 
-# Install Volta
+echo "Installing Volta..."
 curl https://get.volta.sh | bash
 
 # I can't seem to get Volta to work without this
 VOLTA_HOME="$HOME/.volta"
 PATH="$VOLTA_HOME/bin:$PATH"
 grep --silent "$VOLTA_HOME/bin" <<< $PATH || PATH="$VOLTA_HOME/bin:$PATH"
+
+echo "Installing latest Node.js"
+volta install node
+
+echo "Installing tldr"
+volta install tldr
