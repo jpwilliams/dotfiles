4c47a8aab125e99d359337dbc728bab5fbf1aad0
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-06-25 19:35:19 +0100

    Always try to install Linux packages

--- install.sh
+++ install.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  echo "Installing Linux packages..."
+echo "Installing generic packages..."
 
-  # GitHub CLI
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+# GitHub CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
-  sudo apt -y update
-  sudo apt -y install build-essential gh bat exa
-  sudo apt -y upgrade
-fi
+sudo apt -y update
+sudo apt -y install build-essential gh bat exa
+sudo apt -y upgrade
 
 echo "Installing Volta..."
 curl https://get.volta.sh | bash
