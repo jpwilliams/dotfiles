4ac3faebd6bb3ad02f3f56262dad50a974c7b505
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-06-26 20:05:17 +0100

    Add fzf, bat, and ripgrep

--- install.sh
+++ install.sh
@@ -8,7 +8,13 @@ sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 sudo apt -y update
-sudo apt -y install build-essential gh bat exa
+sudo apt -y install build-essential gh exa
+
+# `bat` isn't compatible with `ripgrep` in 20.04
+# Below workaround allows both to be installed
+# See https://github.com/sharkdp/bat/issues/938
+sudo apt -y install -o Dpkg::Options::="--force-overwrite" bat ripgrep
+
 sudo apt -y upgrade
 
 echo "Installing Volta..."
@@ -21,3 +27,8 @@ volta install node
 
 echo "Installing tldr"
 volta install tldr
+
+echo "Installing fzf"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
